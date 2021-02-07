dd16695b21951a3dd19791a5b38e437681628372
Greg Leinwand <gleinwand@bitbucket.org>
2021-02-07 03:13:49 -0700

    data and analyze

--- analyze_prices.py
+++ analyze_prices.py
@@ -1,15 +1,21 @@
 import csv
 import pandas as pd
 import os
-from datetime import datetime
+from datetime import date,datetime
 
 PRE_COVID_START = '2019-06-01'
-PRE_COVED_END = '2020-01-01'
+PRE_COVID_END = '2020-01-01'
 COVID_START = '2020-01-01'
-END_DATE = datetime.today().strftime('%Y-%m-%d')
+BASE_DIR='historical_stock_prices'
 
-base_dir='historical_stock_prices'
-for file in os.listdir(base_dir):
+files = os.listdir(BASE_DIR)
+for file in files[:5]:
     ticker = os.path.splitext(file)[0]
-    df = pd.read_csv(base_dir + '/' + file)
-    print(df)
+    df = pd.read_csv(BASE_DIR + '/' + file)
+
+    df_pre=df.loc[(df['Date'] >= PRE_COVID_START) & (df['Date'] <= PRE_COVID_END)]
+
+    print(f"TICKER: {ticker}")
+    print(f"MIN: {df_pre.min()}")
+    print(f"MAX: {df_pre.max()}")
+    print(f"MEAN: {df_pre.mean()}")
